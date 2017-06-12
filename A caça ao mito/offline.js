95be57c2217d5d17e0c211e717ab012411b62ff3
Vitin157 <vitoralif@yahoo.com.br>
2017-06-12 15:58:44 -0300

    Segundo Envio

---
+++ A caça ao mito/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1497293752,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/cenario-sheet0.png",
+		"images/chao.png",
+		"images/terra-sheet0.png",
+		"images/blocoindio-sheet0.png",
+		"images/indio-sheet0.png",
+		"images/blocoterra-sheet0.png",
+		"images/bloco-sheet0.png",
+		"images/inimigo1-sheet0.png",
+		"images/sensormudarlado-sheet0.png",
+		"images/comida-sheet0.png",
+		"images/nome1-sheet0.png",
+		"images/nome2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
